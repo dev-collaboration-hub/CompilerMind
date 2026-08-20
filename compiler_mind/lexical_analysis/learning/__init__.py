4b7d2608cad65361a3d531fa009a8befad575275
Dilip Singh <commandodilipsingh@gmail.com>
2026-08-20 06:30:43 +0530

    feat: expose lexical learning loop components

--- compiler_mind/lexical_analysis/learning/__init__.py
+++ compiler_mind/lexical_analysis/learning/__init__.py
@@ -1,4 +1,8 @@
+from .critic import Critique, LexicalCritic
 from .experimenter import LexicalExperimenter
+from .hypothesis import IdentifierHypothesis, default_identifier_hypotheses
+from .loop import LearningAttempt, LearningOutcome, LexicalLearningLoop
+from .memory import ExperienceMemory, ExperienceRecord
 from .models import (
     ExpectedToken,
     LexicalTestCase,
@@ -8,10 +12,19 @@ from .models import (
 from .verifier import LexicalVerifier
 
 __all__ = [
+    "Critique",
     "ExpectedToken",
+    "ExperienceMemory",
+    "ExperienceRecord",
+    "IdentifierHypothesis",
+    "LearningAttempt",
+    "LearningOutcome",
+    "LexicalCritic",
     "LexicalExperimenter",
+    "LexicalLearningLoop",
     "LexicalTestCase",
     "LexicalVerifier",
     "VerificationReport",
     "VerificationResult",
+    "default_identifier_hypotheses",
 ]
