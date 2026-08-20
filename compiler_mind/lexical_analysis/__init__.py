1738ae5f3be7da3da864d45043a86a6d94fa0f5b
Dilip Singh <commandodilipsingh@gmail.com>
2026-08-20 06:30:48 +0530

    feat: expose configurable identifier rules

--- compiler_mind/lexical_analysis/__init__.py
+++ compiler_mind/lexical_analysis/__init__.py
@@ -1,5 +1,13 @@
 from .errors import LexicalError
 from .lexer import Lexer
+from .rules import DEFAULT_IDENTIFIER_RULE, IdentifierRule
 from .tokens import Token, TokenKind
 
-__all__ = ["Lexer", "LexicalError", "Token", "TokenKind"]
+__all__ = [
+    "DEFAULT_IDENTIFIER_RULE",
+    "IdentifierRule",
+    "Lexer",
+    "LexicalError",
+    "Token",
+    "TokenKind",
+]
