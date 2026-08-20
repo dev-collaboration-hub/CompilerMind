f2a44b04c7bfbeae2e8bb2f2aece9d996d8f4a82
Dilip Singh <commandodilipsingh@gmail.com>
2026-08-20 06:36:11 +0530

    feat: expose syntax analysis grammar APIs

---
+++ compiler_mind/syntax_analysis/__init__.py
@@ -0,0 +1,12 @@
+from .first_follow import first_of_sequence, first_sets, follow_sets
+from .grammar import ENDMARKER, EPSILON, Grammar, Production
+
+__all__ = [
+    "ENDMARKER",
+    "EPSILON",
+    "Grammar",
+    "Production",
+    "first_of_sequence",
+    "first_sets",
+    "follow_sets",
+]
