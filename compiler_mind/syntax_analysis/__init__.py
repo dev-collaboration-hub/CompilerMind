0c87efcfdb0cb790ec6d2521b2496f7e3093361e
Dilip Singh <commandodilipsingh@gmail.com>
2026-08-20 06:51:03 +0530

    feat: export M4 parser generation APIs

--- compiler_mind/syntax_analysis/__init__.py
+++ compiler_mind/syntax_analysis/__init__.py
@@ -1,5 +1,17 @@
+from .diagnostics import ParserHypothesis, ParserHypothesisGenerator
+from .errors import (
+    LL1Conflict,
+    LL1ConflictError,
+    ParserError,
+    SLRConflict,
+    SLRConflictError,
+)
 from .first_follow import first_of_sequence, first_sets, follow_sets
 from .grammar import ENDMARKER, EPSILON, Grammar, Production
+from .input_adapter import tokens_to_terminals
+from .ll1 import LL1Table, PredictiveParser, RecursiveDescentParser, build_ll1_table
+from .parse_tree import ParseNode
+from .slr import LR0Item, SLRAction, SLRParser, SLRTable, build_slr_table
 from .transform import left_factor, remove_left_recursion
 
 __all__ = [
@@ -12,4 +24,22 @@ __all__ = [
     "follow_sets",
     "left_factor",
     "remove_left_recursion",
+    "LL1Conflict",
+    "LL1ConflictError",
+    "LL1Table",
+    "build_ll1_table",
+    "ParserError",
+    "ParseNode",
+    "PredictiveParser",
+    "RecursiveDescentParser",
+    "LR0Item",
+    "SLRAction",
+    "SLRConflict",
+    "SLRConflictError",
+    "SLRParser",
+    "SLRTable",
+    "build_slr_table",
+    "ParserHypothesis",
+    "ParserHypothesisGenerator",
+    "tokens_to_terminals",
 ]
